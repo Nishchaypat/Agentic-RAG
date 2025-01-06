5489996017ee9aa0cb962263ef4db65807a4ce07
Nishchay Patel <nishchay12120@gmail.com>
2025-01-06 00:13:45 -0600

    Setup completed

---
+++ main.py
@@ -0,0 +1,25 @@
+from langflow.load import run_flow_from_json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def ask_ai(question, profile):
+  TWEAKS = {
+    "TextInput-GesrA": {
+      "input_value": question
+    },
+    "TextInput-4MiiN": {
+      "input_value": profile
+    },
+  }
+
+  result = run_flow_from_json(flow="AskAIV2.json",
+                              input_value="message",
+                              fallback_to_env_vars=True, # False by default
+                              tweaks=TWEAKS)
+
+  return result[0].ouputs[0].result['text'].data['text']
+
+question = "I need to know the best way to build abs and back muscles"
+profile = "Male; 25; 5'10; 180lbs; 3 times a week; 1 hour per session; 1 year of experience; no injuries"
+print(ask_ai(question, profile))
